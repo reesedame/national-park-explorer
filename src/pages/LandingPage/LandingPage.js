074020207683dc2f9af52973fc9e7907b49cf8f8
Reese Dame <reesedame@gmail.com>
2023-06-06 11:30:53 -0700

    Add clickable map to LandingPage

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -1,13 +1,22 @@
 import Header from "../../components/Header/Header";
+import USAMap from "react-usa-map";
+import { Component } from "react";
 
-function LandingPage() {
-	return (
-		<div className="landing-page-container">
-			<Header />
-			<h1>Landing Page</h1>
-			<h2>Map will go here</h2>
-		</div>
-	);
+class LandingPage extends Component {
+	mapHandler = (event) => {
+		alert(event.target.dataset.name);
+	};
+
+	render() {
+		return (
+			<div className="landing-page-container">
+				<Header />
+				<h1>Landing Page</h1>
+				<h2>Map will go here</h2>
+				<USAMap onClick={this.mapHandler} />
+			</div>
+		);
+	}
 }
 
 export default LandingPage;
