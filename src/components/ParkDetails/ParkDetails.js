231f6202afddc2b82afe3db239ccc8a56c025a25
Reese Dame <reesedame@gmail.com>
2023-06-07 14:16:35 -0700

    Park details conditionally render

--- src/components/ParkDetails/ParkDetails.js
+++ src/components/ParkDetails/ParkDetails.js
@@ -1,15 +1,36 @@
+import "./ParkDetails.css";
+
 function ParkDetails({ park }) {
-	return (
-		<div className="park-details">
-			<h3>{park.fullName} Details:</h3>
-			<p>
-				<a href={park.url} target="_blank" rel="noopener noreferrer">
-					Official Site
-				</a>
-			</p>
-			<p>{park.description}</p>
-		</div>
-	);
+	if (Object.keys(park).length !== 0) {
+		return (
+			<div className="park-details">
+				<h1> Details:</h1>
+				<h2>{park.fullName}</h2>
+				<p>
+					<a
+						href={park.url}
+						target="_blank"
+						rel="noopener noreferrer"
+						className="official-site"
+					>
+						Official Site
+					</a>
+				</p>
+				<p>{park.description}</p>
+				<img
+					src={park.images[0].url}
+					alt="View of park"
+					className="park-image"
+				/>
+			</div>
+		);
+	} else {
+		return (
+			<div className="park-details">
+				<h1>Click on a park to view its details!</h1>
+			</div>
+		);
+	}
 }
 
 export default ParkDetails;
