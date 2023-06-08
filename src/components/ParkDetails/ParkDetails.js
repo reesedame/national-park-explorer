69d0a16e34089f91d6f18fa4ad17c87ac368abec
Reese Dame <reesedame@gmail.com>
2023-06-08 13:31:03 -0700

    Update styling for h1s

--- src/components/ParkDetails/ParkDetails.js
+++ src/components/ParkDetails/ParkDetails.js
@@ -3,7 +3,26 @@ import { useState, useEffect } from "react";
 
 function ParkDetails({ park }) {
 	const [imageIdx, setImageIdx] = useState(0);
+	const [amenities, setAmenities] = useState([]);
 	let parkImages = park.images;
+
+	const fetchAmenities = async () => {
+		try {
+			const apiKey = process.env.REACT_APP_KEY;
+			const parkCode = park.parkCode;
+			const requestURL = `https://developer.nps.gov/api/v1/amenities/parksplaces?api_key=${apiKey}&parkCode=${parkCode}`;
+			const response = await fetch(requestURL);
+			const amenitiesDataObject = await response.json();
+			const amenitiesArray = amenitiesDataObject.data;
+			setAmenities(amenitiesArray);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	useEffect(() => {
+		fetchAmenities();
+	}, []);
 
 	useEffect(() => {
 		setInterval(() => {
@@ -38,6 +57,11 @@ function ParkDetails({ park }) {
 					<i>{parkImages[imageIdx].caption}</i>
 				</figcaption>
 			</figure>
+			<ul>
+				{/* {amenities.map((amenity) => {
+					return <li key={amenity[0].id}>{amenity[0].name}</li>;
+				})} */}
+			</ul>
 		</div>
 	);
 }
