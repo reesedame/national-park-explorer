e875b183a76515860dcb90f6c1a87b8a0ea41c48
Reese Dame <reesedame@gmail.com>
2023-06-09 11:14:54 -0700

    ParkAmenities obtained from localStorage

--- src/components/ParkAmenities/ParkAmenities.js
+++ src/components/ParkAmenities/ParkAmenities.js
@@ -1,34 +1,18 @@
-import { useState, useEffect, useCallback } from "react";
-
 function ParkAmenities({ park }) {
-	const [amenities, setAmenities] = useState([]);
-
-	const fetchAmenities = useCallback(async () => {
-		try {
-			const apiKey = process.env.REACT_APP_KEY;
-			const parkCode = park.parkCode;
-			const requestURL = `https://developer.nps.gov/api/v1/amenities/parksplaces?api_key=${apiKey}&parkCode=${parkCode}`;
-			const response = await fetch(requestURL);
-			const amenitiesDataObject = await response.json();
-			const amenitiesArray = amenitiesDataObject.data;
-			setAmenities(amenitiesArray);
-		} catch (error) {
-			return <p>Unable to display amenities. Please try again later!</p>;
-		}
-	}, [park]);
-
-	useEffect(() => {
-		fetchAmenities();
-	}, [fetchAmenities]);
+	const amenities = JSON.parse(localStorage.getItem(park.parkCode));
 
 	return (
 		<div>
 			<h2>Amenities</h2>
-			<ul>
-				{amenities.map((amenity) => {
-					return <li key={amenity[0].id}>{amenity[0].name}</li>;
-				})}
-			</ul>
+			{amenities.length === 0 ? (
+				<h4>Amenity information is not available for this location.</h4>
+			) : (
+				<ul>
+					{amenities.map((amenity) => {
+						return <li key={amenity}>{amenity}</li>;
+					})}
+				</ul>
+			)}
 		</div>
 	);
 }
